e4a21395273630938205de8e4444761aef65611d
notPlancha <andre.plancha@hotmail.com>
2023-10-11 15:05:18 +0100

    reset

--- projeto/receitasdaminhavo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReceitasdaminhavoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "receitasdaminhavo"
--- projeto/receitasdaminhavo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.template.defaulttags import url
-from django.urls import path
-from django.views.generic import RedirectView
-
-from . import views
-urlpatterns = [
-  path("home/", views.index, name="default"),
-  path("", RedirectView.as_view(url='home/'), name="redirectToDefault")
-]
