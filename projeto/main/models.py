79f1650fc56f37c48a1f5e108b4c75d8643fad14
notPlancha <andre.plancha@hotmail.com>
2023-10-17 21:38:19 +0100

    inicio da caixa

--- projeto/main/models.py
+++ projeto/main/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Model
 from django.contrib.auth.models import AbstractUser
 
+DATABASE_READY = False
+
 
 class User(AbstractUser):
   date_created = models.DateTimeField(auto_now_add=True)
@@ -14,9 +16,11 @@ class User(AbstractUser):
   timeout_expires = models.DateTimeField(null=True)
   ban_or_timeout_reason = models.TextField(null=True)
 
+
 class Mod(Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="mod")
   date_turned_mod = models.DateTimeField(auto_now_add=True)
+
 
 class ModPermissions(Model):
   mod = models.ForeignKey(Mod, on_delete=models.CASCADE, related_name="permissions")
@@ -55,22 +59,43 @@ class Thread(Model):
   is_sticky = models.BooleanField(default=False)
   topico = models.ForeignKey('Topico', on_delete=models.CASCADE, null=True, related_name="topico")
 
+
 class Post(Model):
   date_created = models.DateTimeField(auto_now_add=True)
   thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name="thread")
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
   text = models.TextField()
-class Emotes(Model): #TODO make better?
+
+
+class Emotes(Model):  # TODO make better?
   name = models.CharField(max_length=50)
+
   def get_emote_path(self):
     return f"img/emotes/{self.name}.png"
+
 
 class PostEmotes(Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
   emote = models.ForeignKey(Emotes, on_delete=models.CASCADE, related_name="emote")
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emote_user")
 
+
 class Topico(Model):
-
-
   name = models.CharField(max_length=50)
+
+  @dataclass
+  class TopicoData:
+    name: str
+    thread_count: int
+    latest_thread: Thread = None
+
+  @staticmethod
+  def get_topicos() -> list[TopicoData]:
+    if not DATABASE_READY:
+      # return dummy data
+      return [Topico.TopicoData("Test", 1, None)]
+    topicos = []
+    for topico in Topico.objects.all():
+      topicos.append(Topico.TopicoData(topico.name, Thread.objects.filter(topico=topico).count()))
+      topicos[-1].latest_thread = Thread.objects.filter(topico=topico).order_by('-date_created')[0]
+    return topicos
--- projeto/main/views.py
+++ projeto/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from main.models import Topico
 
 
 def index(request):
-
-    return render(request, "index.html", )
+    return render(request, "index.html", {
+        "topicos": Topico.get_topicos()
+    })
