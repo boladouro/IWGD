e592bbb655f8a72adb7b1f268c2c588887a01760
Allan Kardec <kardecallan56@gmail.com>
2023-10-20 16:49:04 +0100

    mexi no design dos post

--- projeto/main/urls.py
+++ projeto/main/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path("a/", views.search_authors, name="search_authors"),
     path("perfil/", views.perfil, name="perfil"),
     path("<str:topico_name>/<int:thread_id>/", views.thread, name="thread"),
-
     path("404/", views.handler404, name="404"),
     path("favicon.ico", RedirectView.as_view(url='/static/img/favicon.ico')),
 
