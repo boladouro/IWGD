ed53ddd6b25f7cdd14b11afe74c17f766a05ad10
notPlancha <andre.plancha@hotmail.com>
2023-10-22 00:41:19 +0100

    changed topico url pra dar bom reverse

--- projeto/main/urls.py
+++ projeto/main/urls.py
@@ -7,7 +7,7 @@ app_name = 'main'
 
 urlpatterns = [
   path("", views.index, name="index"),
-  path("<str:topico_name>/", views.topico, name="topico"),
+  path("t/<str:topico_name>/", views.topico, name="topico"),
   path("a/", views.search_authors, name="search_authors"),
   path("perfil/", views.perfil, name="perfil"),
   path("<str:topico_name>/<int:thread_id>/", views.thread, name="thread"),
